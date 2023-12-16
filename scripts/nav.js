ce83c2d2429c2fa48fe976f1a0f8689a5dcb9cdd
codeandjoy <87646754+codeandjoy@users.noreply.github.com>
2023-12-16 14:13:15 +0200

    Created mobile nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,11 +1,67 @@
-document.querySelector('#btn-about').addEventListener('click', () => {
-    gsap.to(document.body, {duration: .5, scrollTo: '#about-section'});
-});
+const scrollToAbout = () => gsap.to(document.body, {duration: .5, scrollTo: '#about-section'});
+const scrollToAttend = () => gsap.to(document.body, {duration: .5, scrollTo: '#attend-section'});
 
+
+
+// Desktop
+document.querySelector('#btn-about').addEventListener('click', () => {
+    scrollToAbout();
+})
 document.querySelector('#btn-attend').addEventListener('click', () => {
-    gsap.to(document.body, {duration: .5, scrollTo: '#attend-section'});
+    scrollToAttend();
 });
 document.querySelector('#btn-visit').addEventListener('click', () => {
-    gsap.to(document.body, {duration: .5, scrollTo: "#attend-section"})
+    scrollToAttend();
+});
+// 
+
+
+
+// Mobile
+const openMobileMenu = () => {
+    btnOpenNav.classList.add('opened');
+    gsap.to('#full-page-fade', {
+        display: 'block',
+        opacity: .5,
+        duration: .2
+    });
+    gsap.to('.nav-buttons', {
+        height: 'auto',
+        duration: .2
+    });
+    
+    mobileMenuOpen = true;
+}
+const closeMobileMenu = () => {
+    btnOpenNav.classList.remove('opened');
+    gsap.to('#full-page-fade', {
+        display: 'none',
+        opacity: 0,
+        duration: .2
+    });
+    gsap.to('.nav-buttons', {
+        height: 0,
+        duration: .2
+    });
+    
+    mobileMenuOpen = false;
+}
+
+const btnOpenNav = document.querySelector('#btn-open-nav');
+let mobileMenuOpen = false;
+
+btnOpenNav.addEventListener('click', () => {
+    if(!mobileMenuOpen) openMobileMenu();
+    else if (mobileMenuOpen) closeMobileMenu();
 });
 
+document.querySelector('#btn-mobile-about').addEventListener('click', () => {
+    closeMobileMenu();
+    scrollToAbout();
+});
+
+document.querySelector('#btn-mobile-visit').addEventListener('click', () => {
+    closeMobileMenu();
+    scrollToAttend();
+});
+// 
